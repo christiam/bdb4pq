3feeed6e2bdb4b2dbf8cf00d4c382e21b0b51272
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-03-01 16:49:36 -0500

    Provide CLI args

--- src/migrate2pq.py
+++ src/migrate2pq.py
@@ -1,30 +1,58 @@
 #! /usr/bin/env python3
+"""
+migrate2pq.py - See DESC constant below
+
+Author: Christiam Camacho (camacho@ncbi.nlm.nih.gov)
+Created: Fri 01 Mar 2019 04:05:42 PM EST
+"""
 
 from pyspark.sql import SparkSession
+import argparse
+import time
+
+VERSION = '0.1'
+DESC = r"""\
+Pyspark script to migrate CSV files to Parquet
+"""
+IN_DFLT = "gs://camacho-test/nr/nr.01-meta.csv"
+OUT_DFLT = "gs://camacho-test/nr/nr.01-meta.parquet"
+
+def main():
+    parser = create_arg_parser()
+    args = parser.parse_args()
+
+##    spark = SparkSession \
+##        .builder \
+##        .appName("migrate2pq") \
+##        .getOrCreate()
+
+
+    print("Reading {}".format(args.input_file))
+    start = time.time()
+    ##df = spark.read.load(args.input, format="csv", sep="|", inferSchema="true", header="true")
+    end = time.time()
+    print("Reading done in {}".format(end - start))
+
+    print("Writing {}".format(args.out))
+    start = time.time()
+    ##df.write.mode("overwrite").parquet(args.out)
+    end = time.time()
+    print("Writing done in {}".format(end - start))
+    # df.createOrReplaceTempView("pqFile")
+    # human = spark.sql("SELECT * FROM pqFile where taxid == 9606")
+    # human.show()
+    ##spark.stop()
+
+
+def create_arg_parser():
+    """ Create the command line options parser object for this script. """
+    parser = argparse.ArgumentParser(description=DESC)
+    parser.add_argument("input_file", nargs="?", default=IN_DFLT, help="Source file name")
+    parser.add_argument("out", nargs="?", default=OUT_DFLT, help="Output file name")
+    return parser
 
 
 if __name__ == "__main__":
-    spark = SparkSession \
-        .builder \
-        .appName("migrate2pq") \
-        .getOrCreate()
+    import sys
+    sys.exit(main())
 
-
-    file_name = "gs://dataproc-logs-christiam/nr-small-meta.csv"
-    print("Reading {}".format(file_name))
-    df = spark.read.load(file_name, format="csv", sep="|", inferSchema="true", header="true")
-
-    print("Writing WITH select")
-    df.select("accession", "gi", "taxid", "sci_name", "sequence", "slen")\
-        .write.mode("overwrite").save("gs://dataproc-logs-christiam/bdb.parquet", format="parquet")
-
-    print("Writing without select")
-    df.write.mode("overwrite").parquet("gs://dataproc-logs-christiam/nr-small.parquet")
-
-    pqFile = spark.read.parquet("gs://dataproc-logs-christiam/nr-small.parquet")
-    pqFile.createOrReplaceTempView("pqFile")
-    human = spark.sql("SELECT * FROM pqFile where taxid == 9606")
-    human.show()
-
-
-    spark.stop()

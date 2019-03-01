d885f93fe1922e91c2897881ee0f4ed3bbf5766d
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-03-01 18:09:59 -0500

    documentation

---
+++ gcp-utils/run-residue-count.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# gcp-utils/run-residue-count.sh: Counts the residues in a parquet file
+#
+# Author: Christiam Camacho (christiam.camacho@gmail.com)
+# Created: Fri Mar  1 04:13:23 2019
+
+SCRIPT_DIR=$(dirname $0)
+source ${SCRIPT_DIR}/common.sh
+set -euo pipefail
+
+CLUSTER_ID=$($SCRIPT_DIR/get-my-active-cluster-id.sh)
+SRC=$SCRIPT_DIR/../src/residue-count.py
+
+if [ ! -z "$CLUSTER_ID" ] ; then
+    gcloud dataproc jobs submit pyspark $SRC \
+        --cluster ${CLUSTER_ID} \
+        --region ${GCP_REGION} \
+    -- $*
+    #-- -parquet gs://camacho-test/nr/nr.parquet $*
+fi
+

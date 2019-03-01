981c88c2edd1bf0318e2ab8e9d63fe815a8c5704
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-03-01 18:09:25 -0500

    Implement spark job to count residues

--- src/residue-count.py
+++ src/residue-count.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
-rc.py - See DESC constant below
+residue-count.py - See DESC constant below
 
 Author: Christiam Camacho (camacho@ncbi.nlm.nih.gov)
 Created: Fri 01 Mar 2019 04:05:42 PM EST
 """
 import argparse
-import unittest
-import logging
-from contextlib import closing
+import time
+from pyspark.sql import SparkSession
+from operator import add
 
 VERSION = '0.1'
 DESC = r"""\
@@ -16,21 +16,26 @@ Pyspark script to find and count residues
 """
 VALID_RESIDUES = [ '-', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 
     'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'U', '*']
-
-
-class Tester(unittest.TestCase):
-    """ Testing class for this script. """
-    def test_one(self):
-        self.assertTrue(1)
-
+IN_DFLT = "gs://camacho-test/nr/nr.80.parquet"
 
 def main():
     """ Entry point into this program. """
     parser = create_arg_parser()
     args = parser.parse_args()
 
+    spark = SparkSession \
+        .builder \
+        .appName("residue-count") \
+        .getOrCreate()
+
+    df = spark.read.parquet(args.parquet)
+    seqs = df.select("sequence").cache()
+
     for r in args.residue:
-        print(r)
+        start = time.time()
+        num = seqs.rdd.map( lambda s : s.count(r) ).reduce(add)
+        end = time.time()
+        print("Residue {} found {} times in {}".format(r, num, (end-start)))
 
     return 0
 
@@ -39,6 +44,7 @@ def create_arg_parser():
     parser = argparse.ArgumentParser(description=DESC)
     parser.add_argument("residue", nargs="+",
            choices=VALID_RESIDUES, help="Residue(s) to find and count")
+    parser.add_argument("-parquet", default=IN_DFLT, help="Parquet file to search")
     return parser
 
 

8b122d80fe2b69c6f970aee6813618a89e3666e2
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-03-04 11:51:33 -0500

    Fix data generation

--- bin/get-nr-data-fsa.sh
+++ bin/get-nr-data-fsa.sh
@@ -12,10 +12,22 @@ DB=$BLASTDB/nr
 [ -d $OUTPUT_DATA ] || mkdir -p $OUTPUT_DATA
 
 for vol in $(grep DBLIST $DB.pal | sed 's/DBLIST //' | tr -d '"' ); do
-    OUT=$OUTPUT_DATA/$vol-meta.csv
+    OUT=$OUTPUT_DATA/$vol-meta-header.csv
     echo "accession|gi|taxid|sci_name|sequence|slen" > $OUT
 done
 set -x
 parallel --use-cores-instead-of-threads --jobs 50% -q -t --header : --results $OUTPUT_DATA \
     blastdbcmd -entry all -db $BLASTDB/{} \
     -outfmt '%a|%g|%T|%S|%s|%l' -out $OUTPUT_DATA/{}-meta.csv ::: vols $(grep DBLIST $DB.pal | sed 's/DBLIST //' | tr -d '"' )
+
+# Add the header
+TMP=`mktemp -p $PWD -t $(basename -s .sh $0)-XXXXXXX`
+trap " /bin/rm -fr $TMP " INT QUIT EXIT HUP KILL ALRM
+
+for vol in $(grep DBLIST $DB.pal | sed 's/DBLIST //' | tr -d '"' ); do
+    HEADER=$OUTPUT_DATA/$vol-meta-header.csv
+    DATA=$OUTPUT_DATA/$vol-meta.csv
+    cat $HEADER $DATA > $TMP
+    cp $TMP $DATA
+    rm $HEADER
+done

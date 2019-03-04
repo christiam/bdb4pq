7288e3c73e440341951b6681868422aa6e97c8f8
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-03-04 16:10:14 -0500

    Rm needless files

--- bin/common.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# bin/common.sh: Common settings for scripts in this repo
-#
-# Author: Christiam Camacho (camacho@ncbi.nlm.nih.gov)
-# Created: Thu Oct  4 07:51:26 2018
-
-export BLASTDB=$(realpath $(dirname "$0")/../blastdb)

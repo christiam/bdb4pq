9e739a16c17e806349aead4780435f443bf0ff05
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-03-05 14:11:37 -0500

    Add GCP_BUCKET_LOGS

--- gcp-utils/common.sh
+++ gcp-utils/common.sh
@@ -4,8 +4,8 @@
 # Author: Christiam Camacho (camacho@ncbi.nlm.nih.gov)
 # Created: Sat Mar 10 17:24:08 2018
 
-#export PATH=$PATH:$HOME/local/src/google-cloud-sdk/bin
 export GCP_PROJECT=
 export GCP_REGION=us-east4
 export GCP_ZONE=us-east4-b
 export GCP_BUCKET=$USER-test
+export GCP_BUCKET_LOGS=dataproc-logs-$USER

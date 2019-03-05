a5ce5b697e1d7dc25be0b37a71879e9703eece16
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-03-05 18:08:27 -0500

    Tried to fix to run residue-count.py in yarn locally, but failed

--- bin/run-residue-count.sh
+++ bin/run-residue-count.sh
@@ -7,12 +7,22 @@
 SCRIPT_DIR=$(dirname $0)
 set -euo pipefail
 
-SRC=$SCRIPT_DIR/../src/residue-count.py
+MASTER=yarn
+SRC=$(realpath $SCRIPT_DIR/../src/residue-count.py)
 INPUT=hdfs:///user/$USER/nr.80.parquet
+SPARK_HOME=/usr/local/spark/2.3.2
+#JARS=$SPARK_HOME/jars/snappy-0.2.jar,$SPARK_HOME/jars/snappy-java-1.1.2.6.jar
+JARS=$SPARK_HOME/jars/snappy-java-1.1.2.6.jar
+SNAPPY_OPTS='-Dorg.xerial.snappy.lib.name=libsnappyjava.jnilib -Dorg.xerial.snappy.tempdir=/tmp '
 
+set -x
 export SPARK_PRINT_LAUNCH_COMMAND=1
-time spark-submit --master local[2] \
+time spark-submit --master ${MASTER} \
+    --queue prod.blast \
     --name "residue-count" \
     --verbose \
+    --jars ${JARS} \
+    --conf spark.executor.extraJavaOptions="${SNAPPY_OPTS}" \
+    --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
     $SRC -parquet $INPUT $*
 
